5805efd31df13f9d3b776db7b4cc4a9c640485c6
Chris Bolin <bolin.chris@gmail.com>
2016-01-30 15:58:15 -0500

    rm icon switching for now

--- background.js
+++ background.js
@@ -1,14 +1,3 @@
-var imagePaths = {
-  shown: {
-    19: 'icons/shown-19.png',
-    38: 'icons/shown-38.png',
-  },
-  hidden: {
-    19: 'icons/hidden-19.png',
-    38: 'icons/hidden-38.png',
-  },
-};
-
 var createRules = function() {
   // With a new rule ...
   chrome.declarativeContent.onPageChanged.addRules([
@@ -25,22 +14,13 @@ var createRules = function() {
   ]);
 };
 
-var changeIcon = function(tab, hidden){
-  var newImagePaths = hidden ? imagePaths.hidden : imagePaths.shown;
-  chrome.pageAction.setIcon({
-    tabId: tab.id, path: newImagePaths,
-  });
-};
-
 var handlePageActionClick = function(tab) {
   // get the state for this URL and see if it should be hidden or not
   chrome.storage.sync.get(tab.url, function(data){
     var hidden = !(data[tab.url] && data[tab.url].hidden);
     var update = {};
     update[tab.url] = {hidden: hidden};
-    chrome.storage.sync.set(update, function(){
-      changeIcon(tab, hidden);
-    });
+    chrome.storage.sync.set(update);
   });
 };
 
